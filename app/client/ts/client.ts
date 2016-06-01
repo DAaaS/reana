5e4a822f23b9ef57d1f9f943c76f84ebe933d36d
Jody Salt <jody.salt@stfc.ac.uk>
2016-06-01 15:01:59 +0100

    refactor

--- app/client/ts/client.ts
+++ app/client/ts/client.ts
@@ -7,7 +7,14 @@ import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class Client {
 
-	constructor(@Inject(Http) private http: Http, @Inject(CookieService) private cookieService:CookieService){}
+	token: string = null
+
+	constructor(
+		@Inject(Http) private http: Http,
+		@Inject(CookieService) private cookieService:CookieService
+	){
+		this.token = cookieService.get('token');
+	}
 
 	login(username, password){
 		var that = this;
@@ -16,7 +23,8 @@ export class Client {
 			'password=' + encodeURIComponent(password)
 		].join('&');
 		return this.http.get(url).map((result) => {
-			that.cookieService.putObject('token', result.json().token);
+			this.token = result.json().token;
+			that.cookieService.putObject('token', this.token);
 			return;
 		}).catch((error) => {
 			return Observable.throw("Either username or password is incorrect.");
@@ -24,7 +32,7 @@ export class Client {
 	}
 
 	isLoggedIn(){
-		return this.cookieService.get('token') != null;
+		return this.token != null;
 	}
 
 }
