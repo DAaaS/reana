ddea687f554528f4bfbd101e67dc58d01db64630
Jody Salt <jody.salt@stfc.ac.uk>
2016-06-02 13:37:27 +0100

    reverted to angular1

--- app/server/app.js
+++ app/server/app.js
@@ -1,18 +1,19 @@
-import * as express from "express";
-let xmlrpc = require('xmlrpc');
+
+var express = require('express');
+var xmlrpc = require('xmlrpc');
 
 //ignore bad certificates
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 var app = express();
 
-app.use('/', express.static(__dirname + '/../../client/'));
-app.use('/node_modules/', express.static(__dirname + '/../../../../node_modules/'));
+app.use('/', express.static(__dirname + '/../client/'));
+app.use('/bower_components/', express.static(__dirname + '/../../bower_components/'));
 
-app.get('/api/authenticate', (req, res) => {
+app.get('/api/authenticate', function(req, res){
 	var username = req.query.username;
 	var password = req.query.password;
-
+	
 	var client = xmlrpc.createSecureClient({ host: 'hn1.nubes.rl.ac.uk', port: 443, path: '/RPC2'});
 
 	client.methodCall('one.user.login', [username + ":" + password, username, "", -1], function(error, result) {
--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,220 @@
 
-
 var gulp = require('gulp');
 
-gulp.task('client_tsc', ['client_typings'], function(){
-  var ts = require('gulp-typescript');
-  var tsProject = ts.createProject({
-    removeComments: true,
-    target: 'ES3',
-    module: 'commonjs',
-    declarationFiles: false,
-    experimentalDecorators: true
-  });
-  return gulp.src('./app/client/ts/**/**.ts').pipe(ts(tsProject)).js.pipe(gulp.dest('./build/app/client/js'));
-});
-
-gulp.task('server_tsc', ['server_typings'], function(){
-  var ts = require('gulp-typescript');
-  var tsProject = ts.createProject({
-    removeComments: true,
-    target: 'ES3',
-    module: 'commonjs',
-    declarationFiles: false
-  });
-  return gulp.src('./app/server/ts/**/**.ts').pipe(ts(tsProject)).js.pipe(gulp.dest('./build/app/server/js'));
-});
-
-gulp.task('tsc', ['client_tsc', 'server_tsc']);
-
-gulp.task('server_typings', function (callback) {
-  var gulpTypings = require("gulp-typings");
-  return gulp.src("./app/server/ts/typings.json").pipe(gulpTypings());
-});
-
-gulp.task('client_typings', function (callback) {
-  var gulpTypings = require("gulp-typings");
-  return gulp.src("./app/client/ts/typings.json").pipe(gulpTypings());
-});
-
-gulp.task('typings', ['server_typings', 'client_typings']);
-
-gulp.task('sass', function () {
-  var sass = require('gulp-sass');
-  return gulp.src('./app/client/scss/**/*.scss').pipe(sass().on('error', sass.logError)).pipe(gulp.dest('./build/app/client/css'));
-});
-
-gulp.task('copy_html', function() {
-   return gulp.src('./app/client/**/**.html').pipe(gulp.dest('./build/app/client'));
-});
-
-gulp.task('watch', function(){
-  gulp.watch('./app/client/ts/**/**.ts', ['client_tsc']);
-  gulp.watch('./app/server/ts/**/**.ts', ['server_tsc']);
-  gulp.watch('./app/client/scss/**/**.scss', ['sass']);
-  gulp.watch('./app/client/**/**.html', ['copy_html']);
-});
-
-gulp.task('serve', ['watch', 'server_tsc'], function(){
+gulp.task('serve', function(){
   var gls = require('gulp-live-server');
-  var server = gls(['./build/app/server/js/app.js']);
-  gulp.watch(['./build/**/*.css', './build/**/*.js', './build/**/*.html'], function (file) {
+  var server = gls(['app/server/app.js']);
+  gulp.watch(['app/server/**/*.js'], function (file) {
     server.start.bind(server)();
+    server.notify.bind(server)(file);
   });
   server.start();
 });
 
-gulp.task('init', ['tsc', 'sass', 'copy_html']);
 
-gulp.task('default', ['init', 'serve']);
+gulp.task('default', ['serve']);
+
+// // Generated on 2016-06-02 using generator-angular 0.15.1
+// 'use strict';
+
+// var gulp = require('gulp');
+// var $ = require('gulp-load-plugins')();
+// var openURL = require('open');
+// var lazypipe = require('lazypipe');
+// var rimraf = require('rimraf');
+// var wiredep = require('wiredep').stream;
+// var runSequence = require('run-sequence');
+
+// var yeoman = {
+//   app: require('./bower.json').appPath || 'app',
+//   dist: 'dist'
+// };
+
+// var paths = {
+//   scripts: [yeoman.app + '/scripts/**/*.js'],
+//   styles: [yeoman.app + '/styles/**/*.scss'],
+//   test: ['test/spec/**/*.js'],
+//   testRequire: [
+//     yeoman.app + '/bower_components/angular/angular.js',
+//     yeoman.app + '/bower_components/angular-mocks/angular-mocks.js',
+//     yeoman.app + '/bower_components/angular-resource/angular-resource.js',
+//     yeoman.app + '/bower_components/angular-cookies/angular-cookies.js',
+//     yeoman.app + '/bower_components/angular-sanitize/angular-sanitize.js',
+//     yeoman.app + '/bower_components/angular-route/angular-route.js',
+//     'test/mock/**/*.js',
+//     'test/spec/**/*.js'
+//   ],
+//   karma: 'karma.conf.js',
+//   views: {
+//     main: yeoman.app + '/index.html',
+//     files: [yeoman.app + '/views/**/*.html']
+//   }
+// };
+
+// ////////////////////////
+// // Reusable pipelines //
+// ////////////////////////
+
+// var lintScripts = lazypipe()
+//   .pipe($.jshint, '.jshintrc')
+//   .pipe($.jshint.reporter, 'jshint-stylish');
+
+// var styles = lazypipe()
+//   .pipe($.sass, {
+//     outputStyle: 'expanded',
+//     precision: 10
+//   })
+//   .pipe($.autoprefixer, 'last 1 version')
+//   .pipe(gulp.dest, '.tmp/styles');
+
+// ///////////
+// // Tasks //
+// ///////////
+
+// gulp.task('styles', function () {
+//   return gulp.src(paths.styles)
+//     .pipe(styles());
+// });
+
+// gulp.task('lint:scripts', function () {
+//   return gulp.src(paths.scripts)
+//     .pipe(lintScripts());
+// });
+
+// gulp.task('clean:tmp', function (cb) {
+//   rimraf('./.tmp', cb);
+// });
+
+// gulp.task('start:client', ['start:server', 'styles'], function () {
+//   openURL('http://localhost:9000');
+// });
+
+// gulp.task('start:server', function() {
+//   $.connect.server({
+//     root: [yeoman.app, '.tmp'],
+//     livereload: true,
+//     // Change this to '0.0.0.0' to access the server from outside.
+//     port: 9000
+//   });
+// });
+
+// gulp.task('start:server:test', function() {
+//   $.connect.server({
+//     root: ['test', yeoman.app, '.tmp'],
+//     livereload: true,
+//     port: 9001
+//   });
+// });
+
+// gulp.task('watch', function () {
+//   $.watch(paths.styles)
+//     .pipe($.plumber())
+//     .pipe(styles())
+//     .pipe($.connect.reload());
+
+//   $.watch(paths.views.files)
+//     .pipe($.plumber())
+//     .pipe($.connect.reload());
+
+//   $.watch(paths.scripts)
+//     .pipe($.plumber())
+//     .pipe(lintScripts())
+//     .pipe($.connect.reload());
+
+//   $.watch(paths.test)
+//     .pipe($.plumber())
+//     .pipe(lintScripts());
+
+//   gulp.watch('bower.json', ['bower']);
+// });
+
+// gulp.task('serve', function (cb) {
+//   runSequence('clean:tmp',
+//     ['lint:scripts'],
+//     ['start:client'],
+//     'watch', cb);
+// });
+
+// gulp.task('serve:prod', function() {
+//   $.connect.server({
+//     root: [yeoman.dist],
+//     livereload: true,
+//     port: 9000
+//   });
+// });
+
+// gulp.task('test', ['start:server:test'], function () {
+//   var testToFiles = paths.testRequire.concat(paths.scripts, paths.test);
+//   return gulp.src(testToFiles)
+//     .pipe($.karma({
+//       configFile: paths.karma,
+//       action: 'watch'
+//     }));
+// });
+
+// // inject bower components
+// gulp.task('bower', function () {
+//   return gulp.src(paths.views.main)
+//     .pipe(wiredep({
+//       directory: yeoman.app + '/bower_components',
+//       ignorePath: '..'
+//     }))
+//   .pipe(gulp.dest(yeoman.app + '/views'));
+// });
+
+// ///////////
+// // Build //
+// ///////////
+
+// gulp.task('clean:dist', function (cb) {
+//   rimraf('./dist', cb);
+// });
+
+// gulp.task('client:build', ['html', 'styles'], function () {
+//   var jsFilter = $.filter('**/*.js');
+//   var cssFilter = $.filter('**/*.css');
+
+//   return gulp.src(paths.views.main)
+//     .pipe($.useref({searchPath: [yeoman.app, '.tmp']}))
+//     .pipe(jsFilter)
+//     .pipe($.ngAnnotate())
+//     .pipe($.uglify())
+//     .pipe(jsFilter.restore())
+//     .pipe(cssFilter)
+//     .pipe($.minifyCss({cache: true}))
+//     .pipe(cssFilter.restore())
+//     .pipe($.rev())
+//     .pipe($.revReplace())
+//     .pipe(gulp.dest(yeoman.dist));
+// });
+
+// gulp.task('html', function () {
+//   return gulp.src(yeoman.app + '/views/**/*')
+//     .pipe(gulp.dest(yeoman.dist + '/views'));
+// });
+
+// gulp.task('images', function () {
+//   return gulp.src(yeoman.app + '/images/**/*')
+//     .pipe($.cache($.imagemin({
+//         optimizationLevel: 5,
+//         progressive: true,
+//         interlaced: true
+//     })))
+//     .pipe(gulp.dest(yeoman.dist + '/images'));
+// });
+
+// gulp.task('copy:extras', function () {
+//   return gulp.src(yeoman.app + '/*/.*', { dot: true })
+//     .pipe(gulp.dest(yeoman.dist));
+// });
+
+// gulp.task('copy:fonts', function () {
+//   return gulp.src(yeoman.app + '/fonts/**/*')
+//     .pipe(gulp.dest(yeoman.dist + '/fonts'));
+// });
+
+// gulp.task('build', ['clean:dist'], function () {
+//   runSequence(['images', 'copy:extras', 'copy:fonts', 'client:build']);
+// });
+
+// gulp.task('default', ['build']);
