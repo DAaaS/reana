f68d37063f1f81afd1bf0e0b25a69a90e0b8d4a1
Jody Salt <jody.salt@stfc.ac.uk>
2016-06-10 14:59:57 +0100

    added in missing file

---
+++ app/server/src/main/java/uk/ac/rl/reana/cloudclient/Session.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.ac.rl.reana.cloudclient;
+
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
+
+/**
+ *
+ * @author elz24996
+ */
+public class Session extends Entity {
+    
+    private String sessionId;
+    
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public JsonObjectBuilder toJsonObjectBuilder(){
+        JsonObjectBuilder out = Json.createObjectBuilder();
+        out.add("sessionId", getSessionId());
+        return out;
+    }
+}
